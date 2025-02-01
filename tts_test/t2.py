9155097deffe14e771617e26ed337fc8715e0aca
Neil <nj6604053@gmail.com>
2025-02-01 21:28:08 +0530

    new tts

--- tts_test/t2.py
+++ tts_test/t2.py
@@ -1,34 +1,14 @@
-import torch
 from TTS.api import TTS
-from pydub import AudioSegment
-from pydub.playback import play
-import time
-import os
+tts = TTS("tts_models/en/multi-dataset/tortoise-v2")
 
 
-# Get device
-device = "cuda" if torch.cuda.is_available() else "cpu"
+voice_dir=r"C:\Users\njne2\Desktop\Cuda_PWR\CREATIVE\Mini_project\tts_test\tortoise-tts\tortoise\voices"
 
-# List available 🐸TTS models
-print(TTS().list_models())
+print("tts started")
+# Using presets with the same voice
+tts.tts_to_file(text="Good morning my friend, how are you doing today?. hope you are doing great",
+                file_path="output.wav",
+                voice_dir=voice_dir,
+                speaker="lj",
+                preset="ultra_fast")
 
-# Init TTS
-tts = TTS(model_name="tts_models/de/thorsten/tacotron2-DDC", progress_bar=False).to(device)
-
-def talk(text):
-    output_path = r"C:\Users\njne2\Desktop\Cuda_PWR\CREATIVE\Mini_project\tts_test\output.wav"
-    tts.tts_to_file(text="Ich bin eine Testnachricht.", file_path=output_path)
-    if os.path.exists(output_path):
-        audio = AudioSegment.from_wav(output_path)
-        play(audio)
-        print("Audio finished playing.")
-    else:
-        print(f"Error: The file {output_path} was not created.")
-
-
-if __name__ == "__main__":
-    while True:
-        text=str(input("Enter text: "))
-        if text == "exit":
-            break
-        talk(text)
