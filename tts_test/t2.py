a70fd8453738d984daadf72d3a7af9822dec83ca
Neil <nj6604053@gmail.com>
2025-02-01 22:55:03 +0530

    updated tts

--- tts_test/t2.py
+++ tts_test/t2.py
@@ -1,14 +1,21 @@
 from TTS.api import TTS
+import time
 tts = TTS("tts_models/en/multi-dataset/tortoise-v2")
+
 
 
 voice_dir=r"C:\Users\njne2\Desktop\Cuda_PWR\CREATIVE\Mini_project\tts_test\tortoise-tts\tortoise\voices"
 
 print("tts started")
 # Using presets with the same voice
-tts.tts_to_file(text="Good morning my friend, how are you doing today?. hope you are doing great",
-                file_path="output.wav",
-                voice_dir=voice_dir,
-                speaker="lj",
-                preset="ultra_fast")
+while True:
+    tic = time.time()
+    print("started")
+    tts.tts_to_file(text="Good morning my friend, how are you doing today?. hope you are doing great",
+                    file_path="output.wav",
+                    voice_dir=voice_dir,
+                    speaker="lj",
+                    preset="ultra_fast")
+    toc = time.time()
+    print(f"finished{toc-tic}")
 
