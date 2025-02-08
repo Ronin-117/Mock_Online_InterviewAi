99cff0502256bba3d7a6d6eee9fc184efe8fd016
Neil <nj6604053@gmail.com>
2025-02-08 11:16:42 +0530

    interface added

--- conv2.py
+++ conv2.py
@@ -2,6 +2,7 @@ from speach_rec_test.rt_2.t4 import start_listening
 from gem_test.get_resume import get_resume
 from gem_test.t2 import chat_with_model,get_chat
 from tts_test.tortoise_tts.tortoise.read_fast4 import generate_and_stream_audio_from_text
+import time
 
 
 #init gemini code
@@ -14,9 +15,18 @@ generate_and_stream_audio_from_text("Good morning lets get in to it with any del
 print("Start talking")
 for i in range(3):
     transcript=""
+    tick=time.time()
     trans=start_listening()
     for line in trans:
         transcript+=line
+    tock=time.time()
+    print("whisper",tock-tick)
+    tick=time.time()
     resp=chat_with_model(transcript,chat)
+    tock=time.time()
+    print("gem",tock-tick)
+    tick=time.time()
     op_dir=generate_and_stream_audio_from_text(resp)
     print(resp)
+    tock=time.time()
+    print("tts",tock-tick)
--- tts_test/t3.py
+++ tts_test/t3.py
@@ -1,6 +1,10 @@
 from tortoise_tts.tortoise.read_fast4 import generate_and_stream_audio_from_text
+import time
 
 while True:
     text = input("Enter text: ")
-    op_path=generate_and_stream_audio_from_text(text, "lj", 'results/', "op",
+    tick=time.time()
+    op_path=generate_and_stream_audio_from_text(text, "snakes", 'results/', "op",
                                          "standard")
+    tock=time.time()
+    print(tock-tick)
